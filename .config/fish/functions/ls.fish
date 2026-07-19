27b26217140482a39872e46d3e202fc0dc2c313d
Dorrel Redmond <dorrelredmond@gmail.com>
2026-07-19 08:13:44 -0500

    add ls function
    add ls function using eza

---
+++ .config/fish/functions/ls.fish
@@ -0,0 +1,3 @@
+function ls --wraps='eza -lh --no-quotes --time-style long-iso --group-directories-first --color --git --icons' --description 'alias ls eza -lh --no-quotes --time-style long-iso --group-directories-first --color --git --icons'
+    eza -lh --no-quotes --time-style long-iso --group-directories-first --color --git --icons $argv
+end
