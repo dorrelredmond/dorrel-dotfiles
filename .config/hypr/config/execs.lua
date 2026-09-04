deb2b7a30964eb29000dfb6da8c8718e716b319e
Dorrel Redmond <dorrelredmond@gmail.com>
2026-09-04 05:10:44 -0500

    convert llama server to systemd
    convert llama server to systemd

--- .config/hypr/config/execs.lua
+++ .config/hypr/config/execs.lua
@@ -25,7 +25,6 @@ hl.on("hyprland.start", function()
     -- Misc Apps
     hl.exec_cmd("dropbox")
     hl.exec_cmd("sh ~/.config/hypr/utils/gdrive.sh")
-    hl.exec_cmd("sh ~/.config/hypr/utils/llama.sh")
 end)
 
 -- Resizer listeners
--- .config/hypr/utils/llama.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-llama server \
-  --models-dir ~/dev/local-ai-models/ \
-  --host 0.0.0.0 \
-  --port 8080 \
